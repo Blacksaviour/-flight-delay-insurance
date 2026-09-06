90343653560fc7725860afc0e5da5d7159f0309f
Blacksaviour <tomszy91io@gmail.com>
2026-09-06 06:10:28 +0100

    Remove debug logging

--- flight-delay-insurance/frontend/next.config.js
+++ flight-delay-insurance/frontend/next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-console.log("DEBUG NEXT_PUBLIC_FLIGHT_DELAY_REPORTER_ADDRESS =", JSON.stringify(process.env.NEXT_PUBLIC_FLIGHT_DELAY_REPORTER_ADDRESS));
-console.log("DEBUG NEXT_PUBLIC_CREDITCOIN_CHAIN_ID =", JSON.stringify(process.env.NEXT_PUBLIC_CREDITCOIN_CHAIN_ID));
-
 module.exports = {
   reactStrictMode: true,
 };
